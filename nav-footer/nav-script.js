7e7aaf6313c44cd52bf5d975f27c3ff77d01ff38
Félix <fb.felixberger@gmail.com>
2022-03-24 16:41:17 +0100

    last commit on dev

--- nav-footer/nav-script.js
+++ nav-footer/nav-script.js
@@ -3,15 +3,11 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 const navBar = document.querySelector(".navbar");
 let menuOpen = false;
 
-toggleButton.addEventListener("click", () => {
-  navBar.classList.toggle("show-nav");
-
-
 const carousselImg = document.querySelector(".slideshow-container");
 
 toggleButton.addEventListener("click", () => {
-  carousselImg.classList.toggle("hide-caroussel");
   navBar.classList.toggle("show-nav");
+  carousselImg.classList.toggle("hide-caroussel");
 
   if (!menuOpen) {
     toggleButton.classList.add("open");
