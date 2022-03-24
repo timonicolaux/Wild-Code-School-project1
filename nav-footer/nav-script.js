a55dec896160eaaeda105d15ac18829e2b759667
01adrien <adrien.go@outlook.fr>
2022-03-24 14:18:06 +0100

    last commit all good

--- nav-footer/nav-script.js
+++ nav-footer/nav-script.js
@@ -1,6 +1,17 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const navBar = document.querySelector(".navbar");
+let menuOpen = false;
+const carousselImg = document.querySelector(".slideshow-container");
 
 toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
+  carousselImg.classList.toggle("hide-caroussel");
+  navBar.classList.toggle("show-nav");
+  if (!menuOpen) {
+    toggleButton.classList.add("open");
+    menuOpen = true;
+  } else {
+    toggleButton.classList.remove("open");
+    menuOpen = false;
+  }
 });
