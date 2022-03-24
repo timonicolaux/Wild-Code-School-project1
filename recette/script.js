79687e139645e0b4b80362dc2fbefc6c05b6a6bc
Félix <fb.felixberger@gmail.com>
2022-03-24 17:04:27 +0100

    debug

--- recette/script.js
+++ recette/script.js
@@ -1,5 +1,25 @@
 /*jshint esversion: 6 */
 
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const navBar = document.querySelector(".navbar");
+let menuOpen = false;
+
+const carousselImg = document.querySelector(".slideshow-container");
+
+toggleButton.addEventListener("click", () => {
+  navBar.classList.toggle("show-nav");
+  carousselImg.classList.toggle("hide-caroussel");
+
+  if (!menuOpen) {
+    toggleButton.classList.add("open");
+    menuOpen = true;
+  } else {
+    toggleButton.classList.remove("open");
+    menuOpen = false;
+  }
+});
+
 //********************** GESTION DES QUANTITES *******************
 
 function setQuantity(list, nbr, qty) {
